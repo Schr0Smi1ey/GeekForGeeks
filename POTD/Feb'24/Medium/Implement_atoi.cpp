049867723eb6c64aeb73b0b47b1ec09eb988f142
Schrosmiley <radiantremel444@gmail.com>
2024-02-02 15:35:01 +0600

    02.02.23

---
+++ POTD/Feb'24/Medium/Implement_atoi.cpp
@@ -0,0 +1,19 @@
+class Solution{
+  public:
+    /*You are required to complete this method */
+    int atoi(string s) {
+        int ans = 0, n = s.size();
+        for(int i = n - 1,k = 1;i >= 0;i--){
+            if(!isdigit(s[i])){
+                if(s[i] == '-' && !i){
+                    ans *= -1;continue;
+                }
+                return -1;
+            }
+            
+            ans += ((s[i] - '0')*k);
+            k *= 10;
+        }
+        return ans;
+    }
+};
